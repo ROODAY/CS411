b8a1e2dda0e50f14bf06109ea90613ea2ac5f2d4
Rudhra Raveendran <rudytheninja@gmail.com>
2018-04-25 16:38:43 -0400

    mongo test

--- app.js
+++ app.js
@@ -3,6 +3,7 @@ var express = require('express');
 var path = require('path');
 var cookieParser = require('cookie-parser');
 var logger = require('morgan');
+var mongodb = require('mongodb');
 
 var indexRouter = require('./routes/index');
 var queryRouter = require('./routes/query');
@@ -38,6 +39,71 @@ app.use(function(err, req, res, next) {
   // render the error page
   res.status(err.status || 500);
   res.render('error');
+});
+
+let seedData = [
+  {
+    decade: '1970s',
+    artist: 'Debby Boone',
+    song: 'You Light Up My Life',
+    weeksAtOne: 10
+  },
+  {
+    decade: '1980s',
+    artist: 'Olivia Newton-John',
+    song: 'Physical',
+    weeksAtOne: 10
+  },
+  {
+    decade: '1990s',
+    artist: 'Mariah Carey',
+    song: 'One Sweet Day',
+    weeksAtOne: 16
+  }
+];
+
+mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, client) {
+
+  if(err) throw err;
+
+  let db = client.db('dbname')
+  let songs = db.collection('songs');
+  songs.insert(seedData, function(err, result) {
+
+    if(err) throw err;
+
+    /*
+     * Then we need to give Boyz II Men credit for their contribution
+     * to the hit "One Sweet Day".
+     */
+
+    songs.update(
+      { song: 'One Sweet Day' },
+      { $set: { artist: 'Mariah Carey ft. Boyz II Men' } },
+      function (err, result) {
+
+        if(err) throw err;
+
+        /*
+         * Finally we run a query which returns all the hits that spend 10 or
+         * more weeks at number 1.
+         */
+
+        songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
+
+          if(err) throw err;
+
+          docs.forEach(function (doc) {
+            console.log(
+              'In the ' + doc['decade'] + ', ' + doc['song'] + ' by ' + doc['artist'] +
+              ' topped the charts for ' + doc['weeksAtOne'] + ' straight weeks.'
+            );
+          });
+        });
+      }
+    );
+    
+  });
 });
 
 module.exports = app;
--- routes/query.js
+++ routes/query.js
@@ -1,10 +1,11 @@
 var express = require('express');
 var router = express.Router();
 var axios = require('axios');
+var mongodb = require('mongodb');
 
 /* POST query. */
 router.post('/', function (req, res) {
-  axios.get('https://www.eventbriteapi.com/v3/events/search/?q=' + req.body.query +'token=' + process.env.EVENTBRITE_TOKEN)
+  axios.get('https://www.eventbriteapi.com/v3/events/search/?q=' + req.body.query +'&token=' + process.env.EVENTBRITE_TOKEN)
   .then(function (response) {
     res.send(response.data);
   })
@@ -13,5 +14,24 @@ router.post('/', function (req, res) {
     res.send(error);
   });
 })
+
+router.get('/', function(req, res) {
+  mongodb.MongoClient.connect(process.env.MONGODB_URI, function(err, client) {
+    if(err) throw err;
+
+    let db = client.db('dbname')
+    let songs = db.collection('songs');
+
+    songs.find({ weeksAtOne : { $gte: 10 } }).sort({ decade: 1 }).toArray(function (err, docs) {
+      if(err) throw err;
+
+      res.send(docs);
+
+      client.close(function (err) {
+        if(err) throw err;
+      });
+    });
+  });
+});
 
 module.exports = router;
