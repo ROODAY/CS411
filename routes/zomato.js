e593a6e1593ecef665b3b4da6e94a45f383e886c
TengXu <xt@bu.edu>
2018-04-29 13:00:21 -0400

    ADD ZOMATO/EVENTBRITE API
    ADD ZOMATO/EVENTBRITE API

---
+++ routes/zomato.js
@@ -0,0 +1,52 @@
+module.exports = function(app, express, passport) {
+	const router = express.Router();
+	var zomato = require('zomato');
+	var client = zomato.createClient({
+	userKey: Z_USERKEY
+	});
+
+	router.get('/', function(req, res){
+		var city = req.params.cityid;
+		client.getCollections({
+		city_id:"1", //id of the city for which collections are needed 
+		lat:"28.613939", //latitude 
+		lon:"77.209021", //longitude 
+		count:"5" // number of maximum result to display 
+		}, function(err, result){
+			if(!err){
+			  res.send(result);
+			}else {
+			  res.send(err);
+			}
+		});
+	})
+	
+	router.get('/:id', function(req, res){
+		var id = req.params.id;
+		client.getCollections({
+		city_id: id, //id of the city for which collections are needed 
+		count:"5" // number of maximum result to display 
+		}, function(err, result){
+			if(!err){
+			  res.send(result);
+			}else {
+			  res.send(err);
+			}
+		});
+	})
+	
+	router.get('、:city', function(req, res){
+		var city = req.params.city;
+		client.getCities({
+		q: 'Boston'
+		}, function(err, result){
+			if(!err){
+			  res.send(result);
+			}else {
+			  res.send(err);
+			}
+		});
+	})
+
+  return router;
+}
