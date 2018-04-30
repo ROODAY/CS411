95515328749334ab761d9186832d4ba3ab277da4
TengXu <xt@bu.edu>
2018-04-29 21:41:42 -0400

    Update key
    Update userkey

--- routes/zomato.js
+++ routes/zomato.js
@@ -4,10 +4,10 @@ module.exports = function(app, express, passport) {
 	
 	//search for city_id, input is a cityname, return zomato collections in that city
 	router.post('/', function(req, res){
-		axios.get('https://developers.zomato.com/api/v2.1/locations?query=' + req.body.query, {headers: { 'user-key': '34ffc8f7c3a06bd652a07922726d8ed0' }} )
+		axios.get('https://developers.zomato.com/api/v2.1/locations?query=' + req.body.query, {headers: { 'user-key': process.env.Z_USERKEY }} )
 		.then(function (response) {
 			var cityid = JSON.stringify(response.data.location_suggestions[0].city_id);
-		  	axios.get('https://developers.zomato.com/api/v2.1/collections?city_id=' + cityid, {headers: { 'user-key': '34ffc8f7c3a06bd652a07922726d8ed0' }} )
+		  	axios.get('https://developers.zomato.com/api/v2.1/collections?city_id=' + cityid, {headers: { 'user-key': process.env.Z_USERKEY }} )
 			.then(function (response) {
 			  res.send(response.data);
 			})
